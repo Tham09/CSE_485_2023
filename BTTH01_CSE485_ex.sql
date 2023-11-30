850e2c28056dcebddc3d4ccbfed6fb7f79fe5df3
Tran Thi Tham <108069789+Tham09@users.noreply.github.com>
2023-11-30 11:00:54 +0700

    cơ sở dữ liệu

---
+++ BTTH01_CSE485_ex.sql
@@ -0,0 +1,90 @@
+﻿/*a. Liệt kê các bài viết về các bài hát thuộc thể loại Nhạc trữ tình */
+SELECT *
+FROM baiviet
+WHERE ma_tloai = (SELECT ma_tloai FROM theloai WHERE ten_tloai = 'Nhạc trữ tình');
+
+/*b. Liệt kê các bài viết của tác giả “Nhacvietplus”: */
+SELECT *
+FROM baiviet
+WHERE ma_tgia = (SELECT ma_tgia FROM tacgia WHERE ten_tgia = 'Nhacvietplus');
+
+/*c. Liệt kê các thể loại nhạc chưa có bài viết cảm nhận nào: */
+SELECT *
+FROM theloai
+WHERE ma_tloai NOT IN (SELECT DISTINCT ma_tloai FROM baiviet);
+
+/* d. Liệt kê các bài viết với các thông tin sau: mã bài viết, tên bài viết, tên bài hát, tên tác giả, tên thể loại, ngày viết */
+SELECT baiviet.ma_bviet, baiviet.tieude, baiviet.ten_bhat, tacgia.ten_tgia, theloai.ten_tloai, baiviet.ngayviet
+FROM baiviet
+INNER JOIN tacgia ON baiviet.ma_tgia = tacgia.ma_tgia
+INNER JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai;
+
+/* e.Tìm thể loại có số bài viết nhiều nhất: */
+SELECT theloai.ma_tloai, theloai.ten_tloai, COUNT(baiviet.ma_bviet) AS so_bai_viet
+FROM theloai
+LEFT JOIN baiviet ON theloai.ma_tloai = baiviet.ma_tloai
+GROUP BY theloai.ma_tloai
+ORDER BY so_bai_viet DESC
+LIMIT 1;
+
+/* f. Liệt kê 2 tác giả có số bài viết nhiều nhất: */
+SELECT tacgia.ma_tgia, tacgia.ten_tgia, COUNT(baiviet.ma_bviet) AS so_bai_viet
+FROM tacgia
+LEFT JOIN baiviet ON tacgia.ma_tgia = baiviet.ma_tgia
+GROUP BY tacgia.ma_tgia
+ORDER BY so_bai_viet DESC
+LIMIT 2;
+
+/* g. Liệt kê các bài viết về các bài hát có tựa bài hát chứa 1 trong các từ “yêu”, “thương”, “anh”, “em”: */
+SELECT *
+FROM baiviet
+WHERE tieude LIKE '%yêu%' OR tieude LIKE '%thương%' OR tieude LIKE '%anh%' OR tieude LIKE '%em%'
+   OR ten_bhat LIKE '%yêu%' OR ten_bhat LIKE '%thương%' OR ten_bhat LIKE '%anh%' OR ten_bhat LIKE '%em%';
+   
+   
+   
+/* h. Liệt kê các bài viết về các bài hát có tiêu đề bài viết hoặc tựa bài hát chứa 1 trong các từ “yêu”, “thương”, “anh”, “em”: */
+SELECT *
+FROM baiviet
+WHERE tieude LIKE '%yêu%' OR tieude LIKE '%thương%' OR tieude LIKE '%anh%' OR tieude LIKE '%em%'
+   OR ten_bhat LIKE '%yêu%' OR ten_bhat LIKE '%thương%' OR ten_bhat LIKE '%anh%' OR ten_bhat LIKE '%em%';
+   
+/* i.Tạo 1 view có tên vw_Music để hiển thị thông tin về Danh sách các bài viết kèm theo Tên thể loại và tên tác giả */
+CREATE VIEW vw_Music AS
+SELECT baiviet.*, theloai.ten_tloai, tacgia.ten_tgia
+FROM baiviet
+INNER JOIN theloai ON baiviet.ma_tloai = theloai.ma_tloai
+INNER JOIN tacgia ON baiviet.ma_tgia = tacgia.ma_tgia;
+
+
+/* j. Tạo 1 thủ tục có tên sp_DSBaiViet với tham số truyền vào là Tên thể loại và trả về danh sách Bài viết của thể loại đó. Nếu thể loại không tồn tại thì hiển thị thông báo lỗi. */
+CREATE PROCEDURE sp_DSBaiViet (IN ten_tl VARCHAR(50))
+BEGIN
+    DECLARE tl_id INT;
+    SELECT ma_tloai INTO tl_id FROM theloai WHERE ten_tloai = ten_tl;
+    IF tl_id IS NULL THEN
+        SELECT 'Thể loại không tồn tại';
+    ELSE
+        SELECT * FROM baiviet WHERE ma_tloai = tl_id;
+    END IF;
+END;
+
+/* k. Thêm mới cột SLBaiViet vào bảng theloai và tạo trigger tg_CapNhatTheLoai để cập nhật số lượng bài viết: */
+ALTER TABLE theloai ADD COLUMN SLBaiViet INT DEFAULT 0;
+
+CREATE TRIGGER tg_CapNhatTheLoai AFTER INSERT ON baiviet
+FOR EACH ROW
+BEGIN
+    UPDATE theloai SET SLBaiViet = SLBaiViet + 1 WHERE ma_tloai = NEW.ma_tloai;
+END;
+
+/*l. Bổ sung bảng Users để lưu thông tin Tài khoản đăng nhập:*/
+CREATE TABLE Users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
+
+
+
